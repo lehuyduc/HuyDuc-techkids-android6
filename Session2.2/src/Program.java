d2fb532304c85d281a73d3f784959ad95f135faa
ACER\Le Huy Duc <lehuyduc3@gmail.com>
2016-10-06 20:55:49 +0700

    Patch 2.2 changelog: (1) Gameplay: - Computer planes added with features:     + Random movement     + Bullets collision     + Max health = 100     + Disappear upon death
    - Player attack speed cooldown decreased to 0.15s from 0.5s.
    
    /*********************************************************************/
    (2) Developer features added:
    - Completed features for Bullet superclass template (allow more types of Bullet).
    - Completed features for Bot superclass template.
    - Improved objects life-cycle control inside class Object.

---
+++ Session2.2/src/Program.java
@@ -0,0 +1,19 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Le Huy Duc on 02/10/2016.
+ */
+public class Program extends JFrame {
+    public static void main(String[] args) {
+        //System.out.println("Hello world")
+
+        GameWindow gameWindow = new GameWindow();
+        Thread thread = new Thread(gameWindow);
+        thread.start();
+    }
+
+}
