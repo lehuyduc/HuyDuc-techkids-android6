7219d71b8945235b23f25e0b27d6689ac9a11da7
ACER\Le Huy Duc <lehuyduc3@gmail.com>
2016-10-06 20:50:03 +0700

    no message

--- Session2/src/Self.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Le Huy Duc on 03/10/2016.
- */
-public class Self {
-
-    public static int abs(int x) {if (x>=0) return x; else return -x;}
-
-    public static int abs(int x,int y) {return abs(x-y);}
-}
